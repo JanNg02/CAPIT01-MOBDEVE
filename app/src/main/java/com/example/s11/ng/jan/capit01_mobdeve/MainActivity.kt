dbe3e300d413be6e7593183ff776ae118e9b2e45
JanNg02 <114426292+JanNg02@users.noreply.github.com>
2024-05-16 17:31:52 +0800

    this is the mongodb stuff

--- app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/MainActivity.kt
+++ app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.s11.ng.jan.capit01_mobdeve
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnDataFetchedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val news = fetchNews(this)
+        news.execute()
+    }
+
+    override fun onDataFetched(data: List<newsData>) {
+
+        val listView: ListView = findViewById(R.id.listViewTest)
+        val newsDetails = data.map { "${it.announcementTitle}\n Expiry Date: ${it.announcementContent}"}.toTypedArray()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, newsDetails)
+        listView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/newsData.kt
@@ -0,0 +1,3 @@
+package com.example.s11.ng.jan.capit01_mobdeve
+
+data class newsData (val announcementID: String, val announcementTitle: String, val announcementContent: String, val announcementAuthor: String, val announcementUpload: String, val announcementStatus: String)
