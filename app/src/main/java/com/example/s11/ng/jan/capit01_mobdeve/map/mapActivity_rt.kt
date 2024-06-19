211a9dfac6745ee7795b1b71e43f4e1c3960e137
Klaexis <alecklim1245@gmail.com>
2024-06-20 04:16:28 +0800

    Location permissions working (Please try)

--- app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/map/mapActivity_rt.kt
+++ app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/map/mapActivity_rt.kt
@@ -1,17 +1,20 @@
 package com.example.s11.ng.jan.capit01_mobdeve.map
 
+import android.annotation.SuppressLint
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.s11.ng.jan.capit01_mobdeve.R
 import com.example.s11.ng.jan.capit01_mobdeve.map.mapActivity_rt
 import com.example.s11.ng.jan.capit01_mobdeve.help.helpActivity_rt
 import com.example.s11.ng.jan.capit01_mobdeve.file.fileActivity_rt
 import com.example.s11.ng.jan.capit01_mobdeve.dashboard.dashboardActivity_rt
 import com.example.s11.ng.jan.capit01_mobdeve.home.homeActivity_rt
-import com.mapbox.android.core.permissions.PermissionsListener
-import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.maps.MapView
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
@@ -19,21 +22,38 @@ import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.maps.plugin.viewport.viewport
 
 class mapActivity_rt : AppCompatActivity() {
-    lateinit var permissionsManager: PermissionsManager
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.map_rt)
-        val mapView = findViewById<MapView>(R.id.mapView)
 
-        with(mapView) {
-            location.locationPuck = createDefault2DPuck(withBearing = true)
-            location.enabled = true
-            location.puckBearing = PuckBearing.COURSE
-            viewport.transitionTo(
-                targetState = viewport.makeFollowPuckViewportState(),
-                transition = viewport.makeImmediateViewportTransition()
-            )
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                //We can show user a dialog why this permission is necessary
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_REQUEST_CODE
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_REQUEST_CODE
+                )
+            }
         }
 
         val pnabutton: ImageButton = findViewById(R.id.pna_RT)
@@ -62,8 +82,43 @@ class mapActivity_rt : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    @SuppressLint("MissingPermission")
+    private fun enableUserLocation() {
+        val mapView = findViewById<MapView>(R.id.mapView)
+
+        with(mapView) {
+            location.locationPuck = createDefault2DPuck(withBearing = true)
+            location.enabled = true
+            location.puckBearing = PuckBearing.COURSE
+            viewport.transitionTo(
+                targetState = viewport.makeFollowPuckViewportState(),
+                transition = viewport.makeImmediateViewportTransition()
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableUserLocation()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            enableUserLocation()
+        }
     }
 
     fun moveToPnaRT(){
