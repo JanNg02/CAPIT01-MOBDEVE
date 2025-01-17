dead459ad3cae7455e2f19e23fe3c5281813c6f1
Klaexis <alecklim1245@gmail.com>
2025-01-18 05:00:11 +0800

    Proximity check if near evacuation center

--- app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/help/helpActivity_rt.kt
+++ app/src/main/java/com/example/s11/ng/jan/capit01_mobdeve/help/helpActivity_rt.kt
@@ -28,9 +28,13 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import com.example.s11.ng.jan.capit01_mobdeve.R
+import com.example.s11.ng.jan.capit01_mobdeve.map.OnDataFetchedListener
+import com.example.s11.ng.jan.capit01_mobdeve.map.getEvacCenter
+import com.example.s11.ng.jan.capit01_mobdeve.map.modelEvacCenter
 import com.example.s11.ng.jan.capit01_mobdeve.setupFooter_rt
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import okhttp3.MediaType.Companion.toMediaType
@@ -44,6 +48,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -78,12 +83,20 @@ class helpActivity_rt : AppCompatActivity() {
     private var currentSituation = ""
     private var otherSickText = ""
     private var isSickChecked = false
+    private var evacuationCenters: List<modelEvacCenter> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.help_rt)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Fetch evacuation centers
+        getEvacCenter(object : OnDataFetchedListener {
+            override fun onDataFetched(data: List<modelEvacCenter>) {
+                evacuationCenters = data
+            }
+        }).execute()
 
         setCheckBoxes()
 
@@ -144,9 +157,9 @@ class helpActivity_rt : AppCompatActivity() {
 
         val sp = getSharedPreferences("userSession", MODE_PRIVATE)
         val sex = sp.getString("residentSex", "null").toString()
-//        if(sex == "Male") {
-//            pregnantCheckbox.visibility = View.GONE
-//        }
+        if(sex == "Male") {
+            pregnantCheckbox.visibility = View.GONE
+        }
 
         pwdCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
@@ -255,90 +268,159 @@ class helpActivity_rt : AppCompatActivity() {
         fusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
             val location: Location? = task.result
             if (location != null) {
-                val geocoder = Geocoder(this, Locale.getDefault())
-                val addresses: List<Address> =
-                    geocoder.getFromLocation(location.latitude, location.longitude, 1)!!
-                val currentAddress = addresses[0].getAddressLine(0)
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                val nearestEvacCenter = getNearestEvacCenter(currentLatLng)
 
-                //get the currest Session or current User
-                val sp = getSharedPreferences("userSession", MODE_PRIVATE)
-                val fullNameData = sp.getString("residentFullName", "null")
-                val residentEmail = sp.getString("residentEmail", "null")
+                if (nearestEvacCenter != null) {
+                    val distance = calculateDistance(currentLatLng, nearestEvacCenter)
 
-                val fullName = fullNameData.toString()
-                val email = residentEmail.toString()
-                val dateLastSent = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(Date())
-
-                val birthdateString = sp.getString("residentBirthDate", "null")
-                val birthdateCalendar = Calendar.getInstance()
-                birthdateCalendar.time = SimpleDateFormat("MM/dd/yyyy").parse(birthdateString)
-
-                val currentDateCalendar = Calendar.getInstance()
-
-                val year1 = birthdateCalendar.get(Calendar.YEAR)
-                val year2 = currentDateCalendar.get(Calendar.YEAR)
-                var age = year2 - year1
-
-                // adjust for months and days
-                val month1 = birthdateCalendar.get(Calendar.MONTH)
-                val month2 = currentDateCalendar.get(Calendar.MONTH)
-                if (month2 < month1) {
-                    age--
-                } else if (month2 == month1) {
-                    val day1 = birthdateCalendar.get(Calendar.DAY_OF_MONTH)
-                    val day2 = currentDateCalendar.get(Calendar.DAY_OF_MONTH)
-                    if (day2 < day1) {
-                        age--
+                    if (distance <= 150) {
+                        Toast.makeText(
+                            this,
+                            "You are in/near the evacuation center",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Toast.makeText(this, "Distance: $distance Meters", Toast.LENGTH_SHORT).show()
+                        return@addOnCompleteListener // Do not proceed with SOS
+                    } else {
+                        // Proceed with sending SOS data
+                        Toast.makeText(this, "Distance: $distance Meters", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Location Sent", Toast.LENGTH_SHORT).show()
+                        sendSOSData(location)
                     }
                 }
-
-                val sex = sp.getString("residentSex", "null").toString()
-                val teamID = "None"
-                val isFound = false
-
-                val sos = SOS(fullName, email, currentAddress, dateLastSent, age, sex, teamID, isFound, pwdCheckedBoxes, sickCheckedBoxes, pregnantTrimester, currentSituation)
-                val gson = Gson()
-                val json = gson.toJson(sos)
-
-                val requestBody = json.toRequestBody("application/json".toMediaType())
-
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("https://asia-south1.gcp.data.mongodb-api.com/app/mobile_bdrss-fcluenw/endpoint/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-
-                val sosAPI = retrofit.create(sosAPI::class.java)
-
-                sosAPI.postSOS(requestBody).enqueue(object : Callback<ResponseBody> {
-                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                        // handle failure
-                        Log.e("Error", t.message.toString())
-                    }
-
-                    override fun onResponse(
-                        call: Call<ResponseBody>,
-                        response: Response<ResponseBody>
-                    ) {
-                        if (response.isSuccessful) {
-                            val responseBody = response.body()
-                            if (responseBody != null) {
-                                Log.d("Response", responseBody.toString())
-                            } else {
-                                Log.w("Response", "Response body is null")
-                            }
-                        } else {
-                            Log.e("Error", "Response code: ${response.code()}")
-                            Log.e("Error", "Response error message: ${response.message()}")
-                            if (response.errorBody()!= null) {
-                                Log.e("Error", "Response error body: ${response.errorBody()!!.string()}")
-                            }
-                        }
-                    }
-                })
-                Toast.makeText(this, "Location Sent", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "Unable to get current location", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun getLatLngFromAddress(address: String): LatLng? {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        return try {
+            val addressList = geocoder.getFromLocationName(address, 1)
+            if (addressList != null && addressList.isNotEmpty()) {
+                val location = addressList[0]
+                LatLng(location.latitude, location.longitude)
+            } else {
+                null
+            }
+        } catch (e: IOException) {
+            Log.e("Geocoder Error", "Unable to get location from address: $address", e)
+            null
+        }
+    }
+
+    private fun getNearestEvacCenter(currentLocation: LatLng): LatLng? {
+        var nearestCenter: LatLng? = null
+        var minDistance = Int.MAX_VALUE
+
+        for (center in evacuationCenters) {
+            val centerLatLng = getLatLngFromAddress(center.evacAddress) // Convert address to LatLng
+            if (centerLatLng != null) {
+                val distance = calculateDistance(currentLocation, centerLatLng)
+                if (distance < minDistance) {
+                    minDistance = distance
+                    nearestCenter = centerLatLng
+                }
+            }
+        }
+        return nearestCenter
+    }
+
+    private fun calculateDistance(start: LatLng, end: LatLng): Int {
+        val startLocation = Location("start")
+        startLocation.latitude = start.latitude
+        startLocation.longitude = start.longitude
+
+        val endLocation = Location("end")
+        endLocation.latitude = end.latitude
+        endLocation.longitude = end.longitude
+
+        return startLocation.distanceTo(endLocation).toInt() // Distance in meters as Int
+    }
+
+
+    private fun sendSOSData(location: Location) {
+        // Your existing code to send SOS data
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address> = geocoder.getFromLocation(location.latitude, location.longitude, 1)!!
+        val currentAddress = addresses[0].getAddressLine(0)
+
+        // Get the current Session or current User
+        val sp = getSharedPreferences("userSession", MODE_PRIVATE)
+        val fullNameData = sp.getString("residentFullName", "null")
+        val residentEmail = sp.getString("residentEmail", "null")
+
+        val fullName = fullNameData.toString()
+        val email = residentEmail.toString()
+        val dateLastSent = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(Date())
+
+        val birthdateString = sp.getString("residentBirthDate", "null")
+        val birthdateCalendar = Calendar.getInstance()
+        birthdateCalendar.time = SimpleDateFormat("MM/dd/yyyy").parse(birthdateString)
+
+        val currentDateCalendar = Calendar.getInstance()
+
+        val year1 = birthdateCalendar.get(Calendar.YEAR)
+        val year2 = currentDateCalendar.get(Calendar.YEAR)
+        var age = year2 - year1
+
+        // adjust for months and days
+        val month1 = birthdateCalendar.get(Calendar.MONTH)
+        val month2 = currentDateCalendar.get(Calendar.MONTH)
+        if (month2 < month1) {
+            age--
+        } else if (month2 == month1) {
+            val day1 = birthdateCalendar.get(Calendar.DAY_OF_MONTH)
+            val day2 = currentDateCalendar.get(Calendar.DAY_OF_MONTH)
+            if (day2 < day1) {
+                age--
+            }
+        }
+
+        val sex = sp.getString("residentSex", "null").toString()
+        val teamID = "None"
+        val isFound = false
+
+        val sos = SOS(fullName, email, currentAddress, dateLastSent, age, sex, teamID, isFound, pwdCheckedBoxes, sickCheckedBoxes, pregnantTrimester, currentSituation)
+        val gson = Gson()
+        val json = gson.toJson(sos)
+
+        val requestBody = json.toRequestBody("application/json".toMediaType())
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://asia-south1.gcp.data.mongodb-api.com/app/mobile_bdrss-fcluenw/endpoint/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val sosAPI = retrofit.create(sosAPI::class.java)
+
+        sosAPI.postSOS(requestBody).enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                // handle failure
+                Log.e("Error", t.message.toString())
+            }
+
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        Log.d("Response", responseBody.toString())
+                    } else {
+                        Log.w("Response", "Response body is null")
+                    }
+                } else {
+                    Log.e("Error", "Response code: ${response.code()}")
+                    Log.e("Error", "Response error message: ${response.message()}")
+                    if (response.errorBody()!= null) {
+                        Log.e("Error", "Response error body: ${response.errorBody()!!.string()}")
+                    }
+                }
+            }
+        })
     }
 }
